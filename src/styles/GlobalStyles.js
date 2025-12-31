581670ea79ebb8744f2dd7d92fece4c968c11a4a
AhmedZidan399 <ahmed.zidan.new10@gmail.com>
2025-12-31 14:11:23 +0200

    fix login error

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -93,6 +93,10 @@ const GlobalStyles = createGlobalStyle`
   --border-radius-sm: 5px;
   --border-radius-md: 7px;
   --border-radius-lg: 9px;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
 }
 
 *,
